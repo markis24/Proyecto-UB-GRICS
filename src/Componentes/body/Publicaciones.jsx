cb4b2934f4c164f1ee2be428a1086664b95e88fa
Marco Antonio Soliz Lazarte <2023_marco.soliz@iticbcn.cat>
2024-03-21 16:50:45 +0100

    first commit

---
+++ src/Componentes/body/Publicaciones.jsx
@@ -0,0 +1,20 @@
+import Card from '../Cartas/Card.jsx';
+import './Publicaciones.css';
+
+function Publicaciones() {
+    return (
+        <div>
+            <h1>Publicacions</h1>
+            <br />
+            <div><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur voluptatum, porro error itaque voluptates minima pariatur consectetur reprehenderit sed reiciendis!</a></div>
+            <br />
+            <div><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur voluptatum, porro error itaque voluptates minima pariatur consectetur reprehenderit sed reiciendis!</a></div>
+            <br />
+            <div><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur voluptatum, porro error itaque voluptates minima pariatur consectetur reprehenderit sed reiciendis!</a></div>
+            <br />
+            <div><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur voluptatum, porro error itaque voluptates minima pariatur consectetur reprehenderit sed reiciendis!</a></div>
+        </div>
+    );
+}
+
+export default Publicaciones;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './Componentes/Apps/App.jsx';
+import body from './Componentes/body/Publicaciones.jsx'
+import Publicaciones from './Componentes/body/Publicaciones.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <Publicaciones />
     </React.StrictMode>,
 );
