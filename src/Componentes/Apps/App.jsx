6db43ab1644f2e35dee959f9a5697e9967daf56b
Bilal El Moudden <belel22@jaumebalmes.net>
2024-03-21 15:28:28 +0100

    Estructura

---
+++ src/Componentes/Apps/App.jsx
@@ -0,0 +1,15 @@
+import Card from '../Cartas/Card.jsx';
+import './App.css';
+
+function App() {
+    return (
+        <div className='container'>
+            <Card title="Hello World" />
+            <Card title="Hello Bilal" />
+            <Card title="Hello Marco" />
+            <Card title="Hello Marc" />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Componentes/Cartas/Card.jsx
@@ -0,0 +1,13 @@
+import './Card.css'
+
+function Card({ title }) {
+
+    return (
+        <div className="item">
+            <h1>{title}</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium deserunt quidem, aliquid deleniti voluptatibus debitis?</p>
+        </div>
+    )
+}
+
+export default Card
