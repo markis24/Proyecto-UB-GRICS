b4cfd1db5b073bb56afc980716b4c7b988d53c01
Marc <2023_marc.cuzcano@iticbcn.cat>
2024-03-21 16:43:34 +0100

    Modificacion Main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './Componentes/Apps/App.jsx';
+import Form from './Componentes/Formulario Contacto/Form.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <Form/>
     </React.StrictMode>,
 );
