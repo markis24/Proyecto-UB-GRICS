3ae1a301e3308a9a500866b132f9d27eb9a72cd5
Marco Antonio Soliz Lazarte <2023_marco.soliz@iticbcn.cat>
2024-04-02 00:28:50 +0200

    Añadido titol_article

---
+++ src/Componentes/body/Titol_article.jsx
@@ -0,0 +1,14 @@
+import './Titol_article.css';
+
+function Titol_article() {
+    return (
+        <div>
+            <h1>Titol article</h1>
+            <div><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum modi itaque voluptate eligendi voluptatem praesentium nostrum aliquam fugit harum veniam commodi, nisi minima dignissimos architecto ipsum laborum optio quisquam possimus maxime cum, tenetur vero laboriosam nobis quaerat. Neque maiores mollitia blanditiis laudantium? Ipsa asperiores, nostrum veniam in accusantium odit dolorem! </p></div>
+            <h2>Contingut</h2>
+            <div><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum modi itaque voluptate eligendi voluptatem praesentium nostrum aliquam fugit harum veniam commodi, nisi minima dignissimos architecto ipsum laborum optio quisquam possimus maxime cum, tenetur vero laboriosam nobis quaerat. Neque maiores mollitia blanditiis laudantium? Ipsa asperiores, nostrum veniam in accusantium odit dolorem! </p></div>
+            </div>
+    );
+}
+
+export default Titol_article;
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import body from './Componentes/body/Publicaciones.jsx'
 import Publicaciones from './Componentes/body/Publicaciones.jsx';
 import Main_page from './Componentes/body/Titol_projecte.jsx';
 import Titol_projecte from './Componentes/body/Titol_projecte.jsx';
+import Titol_article from './Componentes/body/Titol_article.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
+        <Titol_article />
         <Titol_projecte />
         <Publicaciones />
     </React.StrictMode>,
