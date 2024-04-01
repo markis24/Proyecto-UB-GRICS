dcf1d08c60035aa862ea880520f1ae20197befd0
Marco Antonio Soliz Lazarte <2023_marco.soliz@iticbcn.cat>
2024-04-02 00:22:34 +0200

    Añadido titol_projecte

---
+++ src/Componentes/body/Titol_projecte.jsx
@@ -0,0 +1,17 @@
+import './Titol_projecte.css';
+
+function Titol_projecte() {
+    return (
+        <div>
+            <h1>Titol projecte</h1>
+            <h2>Resum Projecte</h2>
+            <br />
+            <div><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum modi itaque voluptate eligendi voluptatem praesentium nostrum aliquam fugit harum veniam commodi, nisi minima dignissimos architecto ipsum laborum optio quisquam possimus maxime cum, tenetur vero laboriosam nobis quaerat. Neque maiores mollitia blanditiis laudantium? Ipsa asperiores, nostrum veniam in accusantium odit dolorem! </p></div>
+            <h2>Resultats</h2>
+            <br />
+            <div><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum modi itaque voluptate eligendi voluptatem praesentium nostrum aliquam fugit harum veniam commodi, nisi minima dignissimos architecto ipsum laborum optio quisquam possimus maxime cum, tenetur vero laboriosam nobis quaerat. Neque maiores mollitia blanditiis laudantium? Ipsa asperiores, nostrum veniam in accusantium odit dolorem! </p></div>
+            </div>
+    );
+}
+
+export default Titol_projecte;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './Componentes/Apps/App.jsx';
 import body from './Componentes/body/Publicaciones.jsx'
 import Publicaciones from './Componentes/body/Publicaciones.jsx';
+import Main_page from './Componentes/body/Titol_projecte.jsx';
+import Titol_projecte from './Componentes/body/Titol_projecte.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
+        <Titol_projecte />
         <Publicaciones />
     </React.StrictMode>,
 );
