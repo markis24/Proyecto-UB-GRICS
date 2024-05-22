c4ab2e447126adc46b46d606bb3f9e33ddd53a38
Bilal El Moudden <belel22@jaumebalmes.net>
2024-05-22 16:27:08 +0200

    FrontEnd-Final

--- src/Componentes/Proyectos/Proyectos.jsx
+++ src/Componentes/Proyectos/Proyectos.jsx
@@ -19,19 +19,19 @@ function Projectes() {
             <h1>Els Nostres Projectes</h1>
             <div className="imagenes-container">
                 <div className="imagen-wrapper" onClick={() => openInNewTab('/infoprojecte')}>
-                    <img src="https://media.licdn.com/dms/image/C5612AQFakuoHbuWV8Q/article-cover_image-shrink_720_1280/0/1619548985311?e=2147483647&v=beta&t=_0mYX5IOsgc5mK6YhINF72R4XbjLYJ7ZDeq-LQMFrTI" alt="Miembro" />
+                    <img src="https://nosoyasistenta.com/wp-content/uploads/2017/09/Trabajo-social.jpg" alt="Miembro" />
                     <button className="info-boton">Veure Projecte</button>
                 </div>
                 <div className="imagen-wrapper" onClick={() => openInNewTab('/infoprojecte')}>
-                    <img src="https://media.licdn.com/dms/image/C5612AQFakuoHbuWV8Q/article-cover_image-shrink_720_1280/0/1619548985311?e=2147483647&v=beta&t=_0mYX5IOsgc5mK6YhINF72R4XbjLYJ7ZDeq-LQMFrTI" alt="Miembro" />
+                    <img src="https://img.freepik.com/vector-gratis/companeros-piso-comenzando-proyecto-empresarial_23-2148857003.jpg" alt="Miembro" />
                     <button className="info-boton">Veure Projecte</button>
                 </div>
                 <div className="imagen-wrapper" onClick={() => openInNewTab('/infoprojecte')}>
-                    <img src="https://media.licdn.com/dms/image/C5612AQFakuoHbuWV8Q/article-cover_image-shrink_720_1280/0/1619548985311?e=2147483647&v=beta&t=_0mYX5IOsgc5mK6YhINF72R4XbjLYJ7ZDeq-LQMFrTI" alt="Miembro" />
+                    <img src="https://roymo.es/wp-content/uploads/2022/03/proyecto-digital-exito1.jpg" alt="Miembro" />
                     <button className="info-boton">Veure Projecte</button>
                 </div>
                 <div className="imagen-wrapper" onClick={() => openInNewTab('/infoprojecte')}>
-                    <img src="https://media.licdn.com/dms/image/C5612AQFakuoHbuWV8Q/article-cover_image-shrink_720_1280/0/1619548985311?e=2147483647&v=beta&t=_0mYX5IOsgc5mK6YhINF72R4XbjLYJ7ZDeq-LQMFrTI" alt="Miembro" />
+                    <img src="https://www.gob.mx/cms/uploads/article/main_image/112334/21_Dia_nacional_de_las_y_los_trabajadores_sociales-01.jpg" alt="Miembro" />
                     <button className="info-boton">Veure Projecte</button>
                 </div>
                 
